b6a040ca68e657eb02934d5ee255350fb8701f6d
a111r <92301153+a111r@users.noreply.github.com>
2021-11-29 16:14:57 +0300

    Update func1.cpp

--- func1.cpp
+++ func1.cpp
@@ -1,6 +1,6 @@
 #include "easy.h"
 void itc_name() {
-    cout << "Vladislav" << endl;
+    cout << "Shamil" << endl;
 }
 
 void itc_fio() {
@@ -9,16 +9,20 @@ void itc_fio() {
 }
 
 int itc_abs(int num) {
-    if (num < 0) num = -num;
+    if (num < 0)
+	    num = -num;
     return num;
 }
 
-double itc_fabs(double num) {
-    if (num < 0) num = -num;
+double itc_fabs(double num) 
+{
+    if (num < 0) 
+	    num = -num;
     return num;
 }
 
-int itc_revnbr(int num) {
+int itc_revnbr(int num) 
+{
 	return num % 10 * 100 + num % 100 / 10 * 10 + num / 100;
 }
 
