a21fdd850e66536e0e0b1269cd9063f1a8063795
a111r <92301153+a111r@users.noreply.github.com>
2021-11-29 16:17:47 +0300

    Update func2.cpp

--- func2.cpp
+++ func2.cpp
@@ -1,25 +1,38 @@
 #include "easy.h"
-bool itc_iseven(int num) {
-	if (num % 2 == 0) return true;
-	else return false;
+bool itc_iseven(int num)
+{
+	if (num % 2 == 0) 
+		return true;
+	else 
+		return false;
 }
 
-int itc_max(int f_num, int s_num) {
-	if (f_num > s_num) return f_num;
-	else return s_num;
+int itc_max(int a, int b)
+{
+	if (a > b) 
+		return a;
+	else 
+		return b;
 }
 
-int itc_min(int f_num, int s_num) {
-	if (f_num < s_num) return f_num;
-	else return s_num;
+int itc_min(int a, int b)
+{
+	if (a < b) 
+		return a;
+	else 
+		return b;
 }
 
 double itc_fmax(double num, double num1) {
-if (num > num1) return num;
-else return num1;
+if (num > num1) 
+	return num;
+else 
+	return num1;
 }
 
-double itc_fmin(double f_num, double s_num) {
-	if (f_num < s_num) return f_num;
-	else return s_num;
+double itc_fmin(double a, double b) {
+	if (a < b) 
+		return a;
+	else 
+		return b;
 }
