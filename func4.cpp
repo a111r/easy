3cc59601ad4da373eb986ff7c3983646282c9758
a111r <92301153+a111r@users.noreply.github.com>
2021-11-29 16:26:27 +0300

    Update func4.cpp

--- func4.cpp
+++ func4.cpp
@@ -1,17 +1,24 @@
 #include "easy.h"
 double itc_pow(int num, int i) {
-    if (i == 0) return 1;
-    else if(i < 0) return itc_pow(num, i + 1) / num;
-    else return num * itc_pow(num, i - 1);
+    if (i == 0)
+        return 1;
+    else if(i < 0) 
+        return itc_pow(num, i + 1) / num;
+    else 
+        return num * itc_pow(num, i - 1);
 }
 
 bool itc_ispositive(int num) {
-    if (num < 0) return false;
-    else return true;
+    if (num < 0) 
+        return false;
+    else 
+        return true;
 }
 
 bool itc_ispositive_d(double num) {
-    if (num < 0) return false;
-    else return true;
+    if (num < 0) 
+        return false;
+    else
+        return true;
 }
 
